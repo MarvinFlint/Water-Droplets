60df546b245b4a0b2ad57ad78834a23a69428975
Marvin Murmann <m.murmann@nextint.de>
2021-06-29 11:04:01 +0200

    attempted mobile scaling issue

--- sketch.js
+++ sketch.js
@@ -19,7 +19,17 @@ function preload(){
   fontPebble = loadFont("Pebbles.ttf");  
 }
 
+//get DPI
+let dpi = window.devicePixelRatio;
+let canvas = document.getElementsByClassName('p5Canvas');
 
+function fix_dpi() {
+//get CSS height
+//the + prefix casts it to an integer
+//the slice method gets rid of "px"let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);//get CSS width
+let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);//scale the canvascanvas.setAttribute('height', style_height * dpi);
+canvas.setAttribute('width', style_width * dpi);
+}
 
 function setup() {
   frameRate(120);
